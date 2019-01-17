a91864566159bf91319524b41ec6cf24d6dc4f60
Scarlett Ward <Scarlett@Kayde.me>
2019-01-17 18:37:05 +0000

    The basic of bot.

---
+++ KBot/Commands/Util/MiscCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace KBot.Commands.Util
+{
+    class MiscCommands
+    {
+        [Command("ping")]
+        [Description("Example ping command")] 
+        [Aliases("pong")] 
+        public async Task Ping(CommandContext ctx) 
+        {
+            await ctx.TriggerTypingAsync();
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":ping_pong:");
+            await ctx.RespondAsync($"{emoji} Pong! Ping: {ctx.Client.Ping}ms");
+        }
+    }
+}
--- KBot/Program.cs
+++ KBot/Program.cs
@@ -1,12 +1,116 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using KBot.Commands.Util;
+using Newtonsoft.Json;
 
 namespace KBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static DiscordClient discord;
+        private static CommandsNextModule commands;
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //setting up config
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            //setting up the client
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            });
+            discord.Ready += Client_Ready;
+            discord.GuildAvailable += Client_GuildAvailable;
+            discord.ClientErrored += Client_ClientError;
+
+
+            //setting up commands
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = cfgjson.CommandPrefix,
+                EnableDms = true,
+                EnableMentionPrefix = true
+            });
+            commands.CommandExecuted += Commands_CommandExecuted;
+            commands.CommandErrored += Commands_CommandErrored;
+
+
+            //adding the classes which contain commands. May change this to a commandFactory instead of adding 1 million classes here.
+            commands.RegisterCommands<MiscCommands>();
+
+
+
+            //finally, time to connect.
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+
+        private static Task Client_Ready(ReadyEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "KBot", "Client is ready to process events.", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_GuildAvailable(GuildCreateEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "KBot", $"Guild available: {e.Guild.Name}", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_ClientError(ClientErrorEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Error, "KBot", $"Exception occured: {e.Exception.GetType()}: {e.Exception.Message}", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private static Task Commands_CommandExecuted(CommandExecutionEventArgs e)
+        {
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "ExampleBot", $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
+            return Task.CompletedTask;
+        }
+
+        private static async Task Commands_CommandErrored(CommandErrorEventArgs e)
+        {
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "ExampleBot", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+            if (e.Exception is ChecksFailedException ex)
+            {
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000)
+                };
+                await e.Context.RespondAsync("", embed: embed);
+            }
+        }
+
+        public struct ConfigJson
+        {
+            [JsonProperty("token")]
+            public string Token { get; private set; }
+
+            [JsonProperty("prefix")]
+            public string CommandPrefix { get; private set; }
         }
     }
 }
+
